69b840fcd65619528051cbc5756c1bbd3ff9d98b
Dashboard Developer <dev@dashboard.local>
2026-05-18 17:18:15 +0100

    Remove webpack config - use Turbopack by default with bcryptjs pure JS implementation

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Ignore node-pre-gyp HTML files
-    config.module.rules.push({
-      test: /node-pre-gyp[\\/]lib[\\/]util[\\/]nw-pre-gyp/,
-      use: 'ignore-loader',
-    });
-
-    config.module.rules.push({
-      test: /\.html$/,
-      include: /node_modules/,
-      use: 'ignore-loader',
-    });
-
-    // Mark bcrypt as external to avoid bundling issues
-    if (isServer) {
-      config.externals = [...(config.externals || []), 'bcrypt'];
-    }
-
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
