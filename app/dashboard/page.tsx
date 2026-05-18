7de167baf4aa252318f1c85ff5eb655252a0b08a
Dashboard Developer <dev@dashboard.local>
2026-05-18 11:20:27 +0100

    stat

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,27 @@
-import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { Card } from '@/app/ui/dashboard/cards';
 
 export const revalidate = 0;
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  const [revenue, latestInvoices, cardData] = await Promise.all([
+    fetchRevenue(),
+    fetchLatestInvoices(),
+    fetchCardData(),
+  ]);
+
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = cardData;
 
   return (
     <main>
+      <div className="grid gap-6 md:grid-cols-4 mb-6">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+      </div>
       <RevenueChart revenue={revenue} />
       <LatestInvoices latestInvoices={latestInvoices} />
     </main>
